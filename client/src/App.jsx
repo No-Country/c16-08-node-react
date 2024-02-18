023e6f40e2da30d65786c68bb5be5d9f67e75bf5
Evelyn-Nobile <nobileevelyn1@gmail.com>
2024-02-18 18:08:39 -0300

    Se ajustaron algunos detalles de estilos

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,14 @@
+import Navbar from "./components/Navbar/Navbar"
+import Section from "./components/Section/Section"
+
 
 function App() {
 
 
   return (
     <div>
-     
+     <Navbar/>
+     <Section/>
     </div>
   )
 }
