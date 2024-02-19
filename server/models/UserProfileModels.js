8473a0025cc6f1103c6bdfd9cfb9c264a917708c
Qarola <cyan.323@gmail.com>
2024-02-19 16:32:21 -0300

    add User Profile Model

--- server/models/UserProfileModels.js
+++ server/models/UserProfileModels.js
@@ -1,47 +1,27 @@
 import mongoose from 'mongoose';
 
+const { Schema } = mongoose;
 
-const UserSchema = new mongoose.Schema(
+const UserprofileSchema = new Schema(
   {
     name: {
       type: String,
-      required: true,  
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
       required: true,
     },
-    dateOfBirth: {
-     type: Date
+    description: {
+      type: String,
     },
-    verified: {
-      type: Boolean
-    }, 
+    validation: {
+      type: Number, 
+    },
+    commentsQuantity: {
+      type: Number, 
+    },
+    photoProfile: {
+      type: String,
+    },
   },
   { timestamps: true }
 );
-//module.exports = mongoose.model("User", UserSchema); 
-const UserModel = mongoose.model("user", UserSchema )
 
-const createUser = async User => {
-  if (!connected) {
-      console.error('Conexión no establecida');
-      return {};
-  }
-  try {
-      const newUser = new UsersModel(User);
-      await newUser.save();
-      return newUser;
-  } catch (error) {
-      console.error(`Se produjo un error al intentar dar de alta el producto: ${error.message}`);
-      return {};
-  }
-};
-
-
-export default UserModel;
+module.exports = mongoose.model("Userprofile", UserprofileSchema);
