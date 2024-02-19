517aff8d4b72add00f46e698e8c7f5aabb27b64a
Evelyn-Nobile <nobileevelyn1@gmail.com>
2024-02-18 21:17:40 -0300

    Se realizó el footer y se hizo sticky la navbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,53 +1,90 @@
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  const logoURL = 'https://res.cloudinary.com/dpxrcotbh/image/upload/v1708131383/zvfs52j0wfoz5s4cizry.png';
 
-  return ( 
-    <nav className="navbar navbar-expand-md">
+  const logoURL =
+    "https://res.cloudinary.com/dpxrcotbh/image/upload/v1708131383/zvfs52j0wfoz5s4cizry.png";
+
+  return (
+    <nav className="navbar navbar-expand-md" >
       <div className="container-fluid">
-      <img className="navbar-img-toggle" src={logoURL} width="70" height="70" alt="logo" />
+        <img
+          className="navbar-img-toggle"
+          src={logoURL}
+          width="70"
+          height="70"
+          alt="logo"
+        />
 
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-toggler" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbar-toggler"
+          aria-controls="navbarTogglerDemo01"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
 
         <div className="collapse navbar-collapse" id="navbar-toggler">
-
-          <Link className="navbar-brand"to="/" >
-          <img className="navbar-img" src={logoURL} width="70" height="70" alt="logo" />
+          <Link className="navbar-brand" to="/">
+            <img
+              className="navbar-img"
+              src={logoURL}
+              width="70"
+              height="70"
+              alt="logo"
+            />
           </Link>
 
           <ul className="navbar-nav d-flex justify-content-center align-items-center">
-
             <li className="nav-item">
-              <Link className="nav-link active " aria-current="page" to="/">Inicio</Link>
+              <Link className="nav-link active " aria-current="page" to="/">
+                Inicio
+              </Link>
             </li>
 
             <li className="nav-item">
-              <Link className="nav-link active" to="/about">¿Quiénes somos?</Link>
+              <Link className="nav-link active" to="/about">
+                ¿Quiénes somos?
+              </Link>
             </li>
 
             <li className="nav-item">
-              <Link className="nav-link active " to="/explore">Explora</Link>
+              <Link className="nav-link active " to="/explore">
+                Explora
+              </Link>
             </li>
-            
-    <div className="dropdown">
-  <button className="btn btn-secondary dropdown-toggle custom-btn-color" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
- Ingresar
-  </button>
-  <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-    <a className="dropdown-item" href="/login">Iniciar sesión</a>
-    <a className="dropdown-item" href="/signup">Registrarse</a>
-  </div>
-</div>
 
-
+            <div className="dropdown">
+              <button
+                className="btn btn-secondary dropdown-toggle custom-btn-color"
+                type="button"
+                id="dropdownMenuButton"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                Ingresar
+              </button>
+              <div
+                className="dropdown-menu"
+                aria-labelledby="dropdownMenuButton"
+              >
+                <a className="dropdown-item" href="/login">
+                  Iniciar sesión
+                </a>
+                <a className="dropdown-item" href="/signup">
+                  Registrarse
+                </a>
+              </div>
+            </div>
           </ul>
         </div>
       </div>
-    </nav>  
- 
+    </nav>
   );
 };
 
