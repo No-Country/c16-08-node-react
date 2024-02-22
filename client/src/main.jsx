56afb41515e79dda1db69ca84f4de8bd633f5681
Evelyn-Nobile <nobileevelyn1@gmail.com>
2024-02-22 20:44:30 -0300

    Se instala fuente monserrat

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,8 @@ import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import '../build/styles/styles.css'
+import './main.module.css'
+import './Fonts.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
