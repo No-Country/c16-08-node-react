89dfa99f865a52135c73284a3ac3fc8e9bcc555e
Evelyn-Nobile <nobileevelyn1@gmail.com>
2024-02-22 17:39:20 -0300

    Se crea el formulario y se agregan estilos básicos

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route} from "react-router-dom";
 import Login from "./views/Login/Login";
 import Landing from "./views/Landing/Landing";
 import Signup from "./views/Signup/Signup";
+import Form from "./views/Form/Form";
 
 function App() {
   return (
@@ -10,13 +11,14 @@ function App() {
       <div>
         <Routes>
 
-            <Route path='/' element={<Landing />}>    
-      </Route>  
+            <Route path='/' element={<Landing />}></Route>  
 
-<Route path='/login' element={<Login />}>    
-      </Route>
-      <Route path='/signup' element={<Signup />}>    
-      </Route>   
+<Route path='/login' element={<Login />}></Route>
+
+      <Route path='/signup' element={<Signup />}></Route>   
+
+      <Route path='/publicar' element={<Form />}>  </Route>   
+
 </Routes>
       </div>
  /*    </AuthProvider> */
