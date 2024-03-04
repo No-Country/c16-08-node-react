5030f13529216e1ad5fc8ac3219b94553386d6a4
camiloduquee <camiloduquee@gmail.com>
2024-03-04 11:03:46 -0400

    update: cambios

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 import secretKey from "./helpers/generateSecretKey.js"
+import dotenv from "dotenv";
+dotenv.config();
 
-// export const PORT = process.env.PORT || 4000;
-export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
 export const JWT_SECRET_KEY =  secretKey
